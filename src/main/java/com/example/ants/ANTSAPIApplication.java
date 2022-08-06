79a6ed5f7bb4aec9c7f8d9f69125a4c2389fef43
Konippi <konippi-114@outlook.jp>
2022-08-06 11:11:01 +0900

    omit extra files

--- src/main/java/com/example/ants/ANTSAPIApplication.java
+++ src/main/java/com/example/ants/ANTSAPIApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ANTSAPIApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(ANTSAPIApplication.class, args);
     }
-
 }
