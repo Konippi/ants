53d1a5837812a5b04c2836c3fe92b2dbcdf55bc1
chou-dai <c.daiki0916@gmail.com>
2022-08-07 17:25:41 +0900

    configured swagger and frontend

--- src/main/java/com/example/ants/controller/api/SampleController.java
+++ src/main/java/com/example/ants/controller/api/SampleController.java
@@ -1,15 +1,17 @@
-package com.example.ants.controller;
+package com.example.ants.controller.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/api/v1/sample")
 public class SampleController {
-    @GetMapping("/")
+    @GetMapping()
     public ResponseEntity<String> index() {
         return new ResponseEntity<>("Sample", HttpStatus.OK);
     }
