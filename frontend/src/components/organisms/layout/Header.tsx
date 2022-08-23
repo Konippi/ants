abb7e6ffbb0280ff8f8b0b6f3d49946b02d46728
chou-dai <c.daiki0916@gmail.com>
2022-08-22 16:08:52 -1000

    create root componet

---
+++ frontend/src/components/organisms/layout/Header.tsx
@@ -0,0 +1,12 @@
+import {FC, memo} from "react";
+
+const Header: FC = memo(function Header() {
+    
+    return (
+        <header style={{width:"100%", top:0, height:60, background:"red"}}>
+            header
+        </header>
+    );
+});
+
+export default Header;
