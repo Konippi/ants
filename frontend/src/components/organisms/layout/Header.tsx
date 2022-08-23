e6618734e0e887cb13b148c838b137b6e72073c1
chou-dai <c.daiki0916@gmail.com>
2022-08-22 17:30:16 -1000

    install css framework

--- frontend/src/components/organisms/layout/Header.tsx
+++ frontend/src/components/organisms/layout/Header.tsx
@@ -3,7 +3,7 @@ import {FC, memo} from "react";
 const Header: FC = memo(function Header() {
     
     return (
-        <header style={{width:"100%", top:0, height:60, background:"red"}}>
+        <header className="fixed w-full h-16 bg-stone-900 text-white text-center flex items-center justify-center">
             header
         </header>
     );
