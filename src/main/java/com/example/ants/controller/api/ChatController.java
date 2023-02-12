ee584f6150c93ddac0651a30f50d6a9404f6e86f
chou-dai <c.daiki0916@gmail.com>
2023-02-12 20:32:04 +0900

    modified backend

--- src/main/java/com/example/ants/controller/api/ChatController.java
+++ src/main/java/com/example/ants/controller/api/ChatController.java
@@ -1,9 +1,5 @@
 package com.example.ants.controller.api;
 
-import com.example.ants.enums.error.DetailErrorMessage;
-import com.example.ants.enums.error.ErrorCode;
-import com.example.ants.exception.ApiException;
-import com.example.ants.model.request.chat.ChatMessagesRequestBody;
 import com.example.ants.model.request.chat.WebSocketChatMessageRequestBody;
 import com.example.ants.model.response.chat.ChatMessagesResponse;
 import com.example.ants.model.response.chat.WebSocketChatMessageResponse;
@@ -13,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,18 +19,15 @@ import org.springframework.web.bind.annotation.*;
 public class ChatController {
     private final ChatService chatService;
 
-    @GetMapping()
-    public ResponseEntity<ChatMessagesResponse> getChatMessagesByProjectIdList(@RequestBody @Validated ChatMessagesRequestBody requestBody, BindingResult result) throws ApiException {
-        if (result.hasErrors()) {
-            throw new ApiException(ErrorCode.INVALID_QUERY_PARAMETER, DetailErrorMessage.INVALID_QUERY_PARAMETER.getMessage());
-        }
-        return new ResponseEntity<>(chatService.getChatMessagesByProjectIdList(requestBody.getProjectIdList()), HttpStatus.OK);
+    @GetMapping("/{projectIdList}")
+    public ResponseEntity<ChatMessagesResponse> getChatMessagesByProjectIdList(@PathVariable("projectIdList") final List<Integer> projectIdList) {
+        return new ResponseEntity<>(chatService.getChatMessagesByProjectIdList(projectIdList), HttpStatus.OK);
     }
 
     // WebSocket メッセージPOST
     @MessageMapping("/message/{projectId}")
     @SendTo("/chat/message/{projectId}")
-    public WebSocketChatMessageResponse sendMessage(WebSocketChatMessageRequestBody requestBody) throws InterruptedException {
+    public WebSocketChatMessageResponse sendMessage(WebSocketChatMessageRequestBody requestBody) {
         final var projectId = requestBody.getProjectId();
         final var userId = requestBody.getUserId();
         final var message = requestBody.getMessage();
